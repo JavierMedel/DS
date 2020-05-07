3f44765eb5dc770388ea5510fa9174ac52789d44
jmedel <msc.javier.medel@gmail.com>
2020-05-07 09:27:49 -0400

    Linear Search code

--- algorithms.py
+++ algorithms.py
@@ -1,1 +1,16 @@
+idx = -1
 
+def search(nums, n):
+  i = 0
+  while i < len(nums):
+    if nums[i] == n:
+      globals() [idx] = i
+      return True
+    i = i + 1
+  return False
+
+nums = [8, 4, 2, 1, 9, 5]
+n = 2
+
+search(nums, n)
+print('Number ', n, ' found in the position: ', idx)
